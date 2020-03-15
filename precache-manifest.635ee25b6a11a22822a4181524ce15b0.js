7fe150503aaae5d76e48efd879a0a8eb52f118a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-15 14:33:50 +0000

    Deploy Eeenkeeei/blesk-nails to github.com/Eeenkeeei/blesk-nails.git:gh-pages

---
+++ precache-manifest.635ee25b6a11a22822a4181524ce15b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c588fd6782bf45d160071b7335aa48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7020ff8ad590ac7c28bf",
+    "url": "./static/css/main.28496556.chunk.css"
+  },
+  {
+    "revision": "5ea497df13512853c4bb",
+    "url": "./static/js/2.cd276d16.chunk.js"
+  },
+  {
+    "revision": "7020ff8ad590ac7c28bf",
+    "url": "./static/js/main.6a1a1f6a.chunk.js"
+  },
+  {
+    "revision": "356035ea367c6cfd600f",
+    "url": "./static/js/runtime-main.723f8e20.js"
+  },
+  {
+    "revision": "9eaa3aa7f4f9c1ae745d6649a0fbd8dc",
+    "url": "./static/media/vk.9eaa3aa7.svg"
+  }
+]);
